e7c5cfc4ece660274739303fd1bf1c2edb1446b2
Matthew <27matt37@gmail.com>
2018-06-06 23:04:10 +0100

    Fahrenheit to celsius

--- weather/weather.js
+++ weather/weather.js
@@ -1,15 +1,21 @@
 const request = require("request");
 
+var toCelsius = (temperature) => {
+  return parseFloat(((temperature - 32) * (5/9)).toFixed(1));
+};
+
 var getWeather = (lat,long, callback) => {
   request({
     url: `https://api.darksky.net/forecast/3f166f1086c5a25d93bf69d2e5cb12ac/${lat},${long}`,
     json: true
   }, (error, response, body) => {
     if (!error && response.statusCode === 200){
+      var current = body.currently;
       callback(undefined, {
-        temperature: body.currently.temperature,
-        apparentTemperature: body.currently.apparentTemperature,
-        precipProbability: body.currently.precipProbability
+        temperature: toCelsius(current.temperature),
+        apparentTemperature: toCelsius(current.apparentTemperature),
+        precipProbability: current.precipProbability,
+        uvIndex: current.uvIndex
       });
 
     }else {
