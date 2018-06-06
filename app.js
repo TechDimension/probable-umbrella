14612eba87e81b78fe1224b69889c61e4c554787
Matthew <27matt37@gmail.com>
2018-06-06 22:37:08 +0100

    GET / and /:address for finding location based on address

--- app.js
+++ app.js
@@ -1,1 +1,39 @@
-app.js
+const express = require('express');
+const {geoCodeAddress} = require('./geocode/geocode');
+const port = 3000;
+
+var app = express();
+
+app.get('/', (req, res) => {
+  address = req.params.address;
+  geoCodeAddress("EC1V 8BY", (errMessage, results) => {
+    if (errMessage) {
+      console.log(errMessage);
+      return errMessage;
+    }else {
+      console.log(results);
+      res.send(JSON.stringify(results, undefined, 2));
+      return results;
+    }
+  });
+
+});
+
+app.get('/:address', (req, res) => {
+  address = req.params.address;
+  geoCodeAddress(address, (errMessage, results) => {
+    if (errMessage) {
+      console.log(errMessage);
+      return errMessage;
+    }else {
+      console.log(results);
+      res.send(JSON.stringify(results, undefined, 2));
+      return results;
+    }
+  });
+
+});
+
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
+});
