72b625d062df5582ac49e49192efd4d22a0bd467
Matthew Castrillon-Madrigal <27matt37@gmail.com>
2019-10-08 07:02:53 +0100

    Gatsby Start

---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import ClothingSection from "../components/clothingSection"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <ClothingSection>
+      
+    </ClothingSection>
+  </Layout>
+)
+
+export default IndexPage
