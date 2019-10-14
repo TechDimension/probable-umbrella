129eceda00e7f344a3134498115fa6aac3604f56
Matthew Castrillon-Madrigal <27matt37@gmail.com>
2019-10-14 21:23:13 +0100

    Logo on page

--- src/components/header.js
+++ src/components/header.js
@@ -1,32 +1,33 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import Image from './image';
 
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background: `rgb(223, 159, 22)`,
       marginBottom: `1.45rem`,
-      width: '100%',
-      margin: '0 auto',
+      width: '13em',
+      padding: '1em'
     }}
   >
     <div
       style={{
         margin: `0 auto`,
         textAlign:"center",
-        width:'100%',
         padding: `.15rem .1rem`,
       }}
     >
-      <h1 style={{ margin: 0 }}>
+      <h1 style={{ margin: 0, width:'5em' }}>
         <Link
           to="/"
           style={{
-            color: `white`,
+            color: `rebeccapurple`,
             textDecoration: `none`,
           }}
         >
+          <Image style={{width: '50px', display:'inline' }}/>
           {siteTitle}
         </Link>
       </h1>
--- src/components/image.js
+++ src/components/image.js
@@ -13,12 +13,12 @@ import Img from "gatsby-image"
  * - `useStaticQuery`: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-const Image = ({image}) => {
+const Image = () => {
   const data = useStaticQuery(graphql`
     query {
-      placeholderImage: file(relativePath: { eq: "${image}" }) {
+      placeholderImage: file(relativePath: { eq: "umbrella.png" }) {
         childImageSharp {
-          fluid(maxWidth: 300) {
+          fluid(maxWidth: 18000) {
             ...GatsbyImageSharpFluid
           }
         }
